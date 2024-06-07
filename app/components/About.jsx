da254d045b27127629883309ff5abefe3c03c578
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-06-07 15:36:52 +0530

    profile

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,7 +8,9 @@ const AboutMe = () => {
           <div className="lg:w-1/2 lg:flex lg:items-center justify-center">
             <Image
               className="border border-secondary rounded-full hidden sm:block"
-              src="/Profile.JPG"
+              src={`${
+                process.env.NODE_ENV === "production" ? "/Task_next" : ""
+              }/Profile.JPG`}
               alt="profilepicture"
               width="375"
               height="375"
