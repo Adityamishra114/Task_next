0e267c88b6730664677fdd9ba17ebb5fb0906829
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-06-07 15:33:56 +0530

    changes config

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -5,12 +5,12 @@ import { faCodepen, faReact } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="mt-auto py-5 text-center bg-gray-100">
-      <div className="container mx-auto px-4">
+    <footer className="mt-auto w-full py-5 text-center bg-gray-100">
+      <div className="container w-full mx-auto  px-12">
         <h2 className="text-5xl font-extralight pb-3 mt-8 mb-4 text-center">
           Get In Touch
         </h2>
-        <p className="text-lg text-center mt-3 mb-3 pb-3">
+        <p className="text-lg text-center mt-3 mb-5 pb-3">
           Im currently looking for full-time React Developer/Frontend Developer
           opportunities! If you know of any positions available, if you have any
           questions, or if you just want to say hi, please feel free to email me
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
+  assetPrefix: isProd ? "/Task_next/" : "",
   basePath: isProd ? "/Task_next/" : "",
   output: "export",
   distDir: "dist",
